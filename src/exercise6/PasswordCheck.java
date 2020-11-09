663472187782c2499c538b76a44793ba62ba7432
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-11-09 20:12:06 +0000

    2

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,10 +5,33 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-     
-     
+
+      String password = input.nextLine();
+        boolean check = true;
+        int counter = 0;
+
+      for (int index = 0; index <2; index++) {
+        if (Character.isDigit(password.charAt(index))) {
+          counter++;
+        }
+      }
+
+      for (int index = 0; index < password.length(); index++) {
+        if (!Character.isLetterOrDigit(password.charAt(index))) {
+          check = false;
+          break;
+        }
+      }
+
+      if (password.length() >= 8 && counter >= 2 && check) {
+         System.out.println("Password valid!");
+      } else {
+         System.out.println("Password invalid!");
+      }
     }
 }
+     
+     
 
 
 
@@ -23,6 +46,3 @@ public class PasswordCheck {
 
 
 
-
-    }
-}
